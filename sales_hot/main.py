9a7723697463cc8cf8fde1cd9e49222374830e96
Vitor Martins <vitor.barbosa@a3data.com.br>
2023-06-29 02:27:45 -0300

    update code

---
+++ sales_hot/main.py
@@ -0,0 +1,16 @@
+import select_base as sb
+import numpy as np
+import pandas as pd
+
+class read_data:
+    
+    def __init__(self,csv_data=True):
+        print('\n Reading data... \n')
+        if csv_data:
+            self.df = pd.read_csv('data/sales_data.csv',index_col=0)
+        else:
+            SQL_conn = sb.read_data_SQL()
+            self.df = SQL_conn.read_table(save_csv=False)
+
+if __name__ == '__main__':
+    rd = read_data()
---
+++ sales_hot/select_base.py
@@ -0,0 +1,44 @@
+import mysql.connector
+import pandas as pd
+
+
+class read_data_SQL:
+    
+    def __init__(self,host='interview-2.ck1h5ksgzpiq.us-east-1.rds.amazonaws.com',port='3306',user='hotinterview',password='6cT4jk9QWPhQC9KXWKDd',database='innodb'):
+        
+        self.cnx = mysql.connector.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password,
+            database=database
+        )
+
+    def read_table(self, table_name = 'sales_data',save_csv=False):
+        print('\n *** Reading table ' + table_name + ' ***')
+        cursor = self.cnx.cursor()
+
+        query = 'SELECT * FROM ' + table_name
+        cursor.execute(query)
+
+        rows = cursor.fetchall()
+
+        columns = [desc[0] for desc in cursor.description]
+
+        df = pd.DataFrame(rows, columns=columns)
+
+        if save_csv:
+            df.to_csv('data/sales_data.csv')
+
+        cursor.close()
+        self.cnx.close()
+
+        return df
+
+if __name__ == '__main__':
+    print('\n Begin... \n')
+
+    SQL_conn = read_data_SQL()
+    df = SQL_conn.read_table(save_csv=False)
+
+    print('\n Finish! \n')
